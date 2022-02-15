bfabd01305e230f8abf122eff47a7904dfd596c0
Yuki Uchiyama <uchiyama.yuki@moneyforward.co.jp>
2022-02-15 23:15:04 +0900

    Can access content page from index

--- pages/index.js
+++ pages/index.js
@@ -2,19 +2,8 @@ import Head from 'next/head'
 import Layout, { siteTitle } from '../components/layout'
 import utilStyles from '../styles/utils.module.css'
 import { getSortedPostsData } from '../lib/posts'
-import { getAllPostIds } from '../lib/posts'
-
-export async function getStaticProps() {
-  const paths = getAllPostIds()
-  const allPostsData = getSortedPostsData()
-  return {
-    props: {
-      allPostsData
-    },
-    paths,
-    fallback: false
-  }
-}
+import Link from 'next/link'
+import Date from '../components/date'
 
 export default function Home({ allPostsData }) {
   return (
@@ -35,15 +24,26 @@ export default function Home({ allPostsData }) {
         <ul className={utilStyles.list}>
           {allPostsData.map(({ id, date, title }) => (
             <li className={utilStyles.listItem} key={id}>
-              {title}
+              <Link href={`/posts/${id}`}>
+                <a>{title}</a>
+              </Link>
               <br />
-              {id}
-              <br />
-              {date}
+              <small className={utilStyles.lightText}>
+                <Date dateString={date} />
+              </small>
             </li>
           ))}
         </ul>
       </section>
     </Layout>
   )
+}
+
+export async function getStaticProps() {
+  const allPostsData = getSortedPostsData()
+  return {
+    props: {
+      allPostsData
+    }
+  }
 }
